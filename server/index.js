b62d40dce5ba8914a9ea304c70b9196d14c721f9
Inna Botchkina <innabobolina@yahoo.com>
2022-08-22 21:20:24 -0400

    add server, connect to Mongodb, add localhost:8000/users

--- server/index.js
+++ server/index.js
@@ -1,8 +1,32 @@
 const express = require('express');
-const app = express();
-const PORT = 8000;
-app.listen(PORT, () => console.log('Server running on PORT ' + PORT));
+const {MongoClient} = require('mongodb')
+require('dotenv').config()
+
+const uri = process.env.URI
+const app = express()
 
 app.get('/', (req, res) => {
     res.json('Hello to my app')
 })
+
+app.post('/signup', (req, res) => {
+    res.json('Hello to my app')
+})
+
+app.get('/users', async (req, res) => {
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        const database = client.db('app-data');
+        const users = database.collection("users");
+        const returnedUsers = await users.find().toArray();
+        res.send(returnedUsers);
+    }
+    finally {
+        await client.close();
+    }
+})
+
+const PORT = 8000;
+app.listen(PORT, () => console.log('Server running on PORT ' + PORT));
