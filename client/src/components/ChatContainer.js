e0b22b2bca3d2138df9cd87659f2c411819ec77d
ramblelob <aileen.ramelb@gmail.com>
2022-11-20 10:27:43 -0800

    Updated chat container with UseState

--- client/src/components/ChatContainer.js
+++ client/src/components/ChatContainer.js
@@ -2,19 +2,20 @@ import React from 'react';
 import ChatHeader from './ChatHeader';
 import MatchesDisplay from './MatchesDisplay';
 import ChatDisplay from './ChatDisplay';
+import {useState} from 'react';
 
-const ChatContainer = ({ user }) => {
+const ChatContainer = ({user}) => {
+  const [ clickedUser, setClickedUser ] = useState(null)
+  console.log("clickUser", clickedUser);
   return (
     <div className="chat-container">
-      <ChatHeader user={ user } />
-
+      <ChatHeader user={user} />
       <div>
-        <button className="option">Matches</button>
-        <button className="option">Chat</button>
+        <button className="option" onClick={() => setClickedUser(null)}>Matches</button>
+        <button className="option" disabled={!clickedUser}>Chat</button>
       </div>
-
-      <MatchesDisplay matches={user.matches}/>
-      <ChatDisplay />
+      {!clickedUser && <MatchesDisplay matches={user.matches} setClickedUser={setClickedUser} />}
+      {clickedUser && <ChatDisplay user={user} clickedUser={clickedUser} />}
     </div>
 
   )
